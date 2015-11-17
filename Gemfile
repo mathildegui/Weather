1b0a0adfaecf6e9e65d2adf336bbff1a7d11305d
mathildegui <mathilde.guillossou@gmail.com>
2015-11-17 20:15:37 +0100

    add gemfile and update controller

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'rails-api'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
@@ -45,6 +45,6 @@ group :development do
   gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
+  #gem 'spring'
 end
 
--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,64 +1,70 @@
 class WeathersController < ApplicationController
-  before_action :set_weather, only: [:show, :edit, :update, :destroy]
+  before_action :set_weather, only: [:show, :update, :destroy]
 
   # GET /weathers
   # GET /weathers.json
   def index
     @weathers = Weather.all
+    render json: @weathers
   end
 
   # GET /weathers/1
   # GET /weathers/1.json
   def show
+    render json: @weather
   end
 
   # GET /weathers/new
-  def new
-    @weather = Weather.new
-  end
+  #def new
+  #  @weather = Weather.new
+  #end
 
   # GET /weathers/1/edit
-  def edit
-  end
+  #def edit
+  #end
 
   # POST /weathers
   # POST /weathers.json
   def create
     @weather = Weather.new(weather_params)
 
-    respond_to do |format|
+    #respond_to do |format|
       if @weather.save
-        format.html { redirect_to @weather, notice: 'Weather was successfully created.' }
-        format.json { render :show, status: :created, location: @weather }
+        render json: @weather, status: :created, location: @weather
+        #format.html { redirect_to @weather, notice: 'Weather was successfully created.' }
+        #format.json { render :show, status: :created, location: @weather }
       else
-        format.html { render :new }
-        format.json { render json: @weather.errors, status: :unprocessable_entity }
+        render json: @weather.errors, status: :unprocessable_entity
+        #format.html { render :new }
+        #format.json { render json: @weather.errors, status: :unprocessable_entity }
       end
-    end
+    #end
   end
 
   # PATCH/PUT /weathers/1
   # PATCH/PUT /weathers/1.json
   def update
-    respond_to do |format|
+    #respond_to do |format|
       if @weather.update(weather_params)
-        format.html { redirect_to @weather, notice: 'Weather was successfully updated.' }
-        format.json { render :show, status: :ok, location: @weather }
+        render json: @weather
+        #format.html { redirect_to @weather, notice: 'Weather was successfully updated.' }
+        #format.json { render :show, status: :ok, location: @weather }
       else
-        format.html { render :edit }
-        format.json { render json: @weather.errors, status: :unprocessable_entity }
+        render json: @weather.errors, status: unprocessable_entity
+        #format.html { render :edit }
+        #format.json { render json: @weather.errors, status: :unprocessable_entity }
       end
-    end
+    #end
   end
 
   # DELETE /weathers/1
   # DELETE /weathers/1.json
   def destroy
     @weather.destroy
-    respond_to do |format|
-      format.html { redirect_to weathers_url, notice: 'Weather was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    #respond_to do |format|
+    #  format.html { redirect_to weathers_url, notice: 'Weather was successfully destroyed.' }
+    #  format.json { head :no_content }
+    #end
   end
 
   private
